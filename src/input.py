e93cc1b64626e6f9593f6f71b2f7cb8abbad5aec
DanielCarneiro123 <danielmc2116@gmail.com>
2024-03-03 19:44:24 +0000

    small update, still no good

--- src/input.py
+++ src/input.py
@@ -13,12 +13,26 @@ class Airplane:
         self.actual_landing_time = 0 
         self.priority = 0
 
+    def get_arriving_fuel_level(self):
+        return self.arriving_fuel_level
+
+    def get_fuel_consumption_rate(self):
+        return self.fuel_consumption_rate
+
+    def get_expected_landing_time(self):
+        return self.expected_landing_time
+
+    def get_actual_landing_time(self):
+        return self.actual_landing_time
+
+    def get_priority(self):
+        return self.priority
+
     def __str__(self):
         return f"Fuel level: {self.arriving_fuel_level}, Fuel consumption rate: {self.fuel_consumption_rate}, Expected landing time: {self.expected_landing_time}, Actual landing time: {self.actual_landing_time}"
         
 
     def ensure_enough_fuel(self):
-        # Calculate fuel needed for one hour of flight
         fuel_needed = self.fuel_consumption_rate * 60
         if self.arriving_fuel_level < fuel_needed:
             self.priority = 1000  # Prioritize safety if fuel is insufficient
@@ -47,39 +61,46 @@ class LandingStrip:
 
     def get_current_airplanes(self):
         return self.current_airplanes
+    
 
     def land_airplane(self, airplane, landing_strips):
-        
         if airplane.expected_landing_time >= self.empty_time:
-            # If the expected landing time is after the current empty time, no waiting is needed
+            # Se o tempo de pouso esperado for após o tempo vazio atual, não há necessidade de esperar
             actual_landing_time = airplane.expected_landing_time
             self.empty_time = actual_landing_time + 3
             airplane.actual_landing_time = actual_landing_time
+            airplane.ensure_enough_fuel()
             self.add_airplane(airplane)
         else:
-            # If the expected landing time is before the current empty time, adjust accordingly
+            # Se o tempo de pouso esperado for antes do tempo vazio atual, ajuste-o de acordo
             max_empty_time = max(landing_strip.get_empty_time() for landing_strip in landing_strips)
             min_empty_time_strip = min(range(len(landing_strips)), key=lambda i: landing_strips[i].get_empty_time())
             min_empty_time = landing_strips[min_empty_time_strip].get_empty_time()
             actual_landing_time = max(max_empty_time - 3, max(airplane.expected_landing_time, min_empty_time))
             landing_strips[min_empty_time_strip].set_empty_time(actual_landing_time + 3)
             airplane.actual_landing_time = actual_landing_time
+            airplane.ensure_enough_fuel()
             landing_strips[min_empty_time_strip].add_airplane(airplane)
+
 
 def cost_function(solution):
     cost = 0
     for landing_strip in solution:
         for airplane in landing_strip:
-            delay = airplane.actual_landing_time - airplane.expected_landing_time
-            if delay > 0 and airplane.priority == 1:
+            print(airplane.get_actual_landing_time())
+            print(airplane.get_expected_landing_time())
+            print(airplane.get_priority())
+            delay = airplane.get_actual_landing_time() - airplane.get_expected_landing_time()
+            if delay > 0 and airplane.get_priority() == 1:
                 cost += delay
-            elif airplane.priority > 1:
-                cost += airplane.priority  
-            elif delay > 0 and airplane.priority > 1:
-                cost += delay * airplane.priority
+            elif airplane.get_priority() > 1:
+                cost += airplane.get_priority()
+            elif delay > 0 and airplane.get_priority() > 1:
+                cost += delay * airplane.get_priority()
             else:
-                cost += airplane.priority
+                cost += airplane.get_priority()
     return cost
+
 
 
 
@@ -96,6 +117,7 @@ def generate_initial_solution(airplanes, landing_strips):
 def generate_neighbors(airplanes):
     neighbors = []
     num_airplanes = len(airplanes)
+
     
     # Iterate through all possible pairs of airplanes
     for i in range(num_airplanes):
@@ -109,12 +131,13 @@ def generate_neighbors(airplanes):
     return neighbors
 
 
-def simulated_annealing(airplanes, landing_strips, initial_temperature=1000, cooling_rate=0.99, stopping_temperature=0.0001, max_iterations=1000):
+def simulated_annealing(airplanes, landing_strips, initial_temperature=1000, cooling_rate=0.00001, stopping_temperature=0.0001, max_iterations=1000):
     current_solution = generate_initial_solution(airplanes, landing_strips)
     for strip in current_solution:
             for airplane in strip:
                 landing_strip = random.choice(landing_strips)
                 landing_strip.land_airplane(airplane, landing_strips)
+    generate_neighbors(current_solution)
     current_cost = cost_function(current_solution)
     best_solution = current_solution
     best_cost = current_cost
@@ -122,7 +145,7 @@ def simulated_annealing(airplanes, landing_strips, initial_temperature=1000, coo
     iteration = 0
     
     while temperature > stopping_temperature and iteration < max_iterations:
-        new_solution = copy.deepcopy(current_solution)  # Mova esta linha aqui dentro do loop while
+        new_solution = copy.deepcopy(current_solution)  
         for strip in new_solution:
             for airplane in strip:
                 landing_strip = random.choice(landing_strips)
@@ -130,6 +153,7 @@ def simulated_annealing(airplanes, landing_strips, initial_temperature=1000, coo
 
         generate_neighbors(new_solution)
         new_cost = cost_function(new_solution)
+        print(new_cost)
         delta_cost = new_cost - current_cost
         
         if delta_cost < 0 or math.exp(-delta_cost / temperature) > random.random():
@@ -137,6 +161,7 @@ def simulated_annealing(airplanes, landing_strips, initial_temperature=1000, coo
             current_cost = new_cost
             
             if new_cost < best_cost:
+                print("here")
                 best_solution = new_solution
                 best_cost = new_cost
                 
