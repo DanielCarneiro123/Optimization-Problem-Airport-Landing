7f6089663d1eb0ff002b8e909752eb7f36119c35
elliothacker33 <tomastorrespereira@gmail.com>
2024-03-24 17:05:20 +0000

    added dependencies on best solution

--- src/hill.py
+++ src/hill.py
@@ -8,28 +8,35 @@ from tabu_search import *
     
 def hill_climbing(airplanes, max_iterations=1000):
     current_solution = generate_initial_solution(airplanes)
-    _,current_cost,_,_ = generateResults(current_solution)
+    best_solution = current_solution
+    best_solution_value = float('inf')  
+    unsafe_planes_sol = 100000  
+    num_of_crashes_sol = 100000
     iteration = 0
 
     while iteration < max_iterations:
         neighbors = generate_neighbors(current_solution)
-        cost_neighbor = float('-inf')
         best_neighbor = None
+        best_neighbor_value = float('inf')
+        num_of_crashes = 100000
+        unsafe_planes = 100000 
         for neighbor in neighbors:
-            _,neighbor_value,_,_ = generateResults(neighbor)
-
-            if neighbor_value < cost_neighbor:
-                current_solution = neighbor
-                cost_neighbor = neighbor_value
+            landing_strips, neighbor_value, unsafe_waiting, curr_num_of_crashes = generateResults(neighbor)
+            if (((neighbor_value < best_neighbor_value) and (unsafe_waiting <= unsafe_planes) and (curr_num_of_crashes<=num_of_crashes)) or ((unsafe_waiting<unsafe_planes) and (curr_num_of_crashes <= num_of_crashes) ) or (curr_num_of_crashes < num_of_crashes)):
+                best_neighbor_value = neighbor_value
                 best_neighbor = neighbor
+                unsafe_planes = unsafe_waiting
+                num_of_crashes = curr_num_of_crashes
 
-
-        if (cost_neighbor <= current_cost):
-            return current_solution,current_cost
-        current_solution = best_neighbor
-        current_cost = cost_neighbor
+        if ((best_neighbor_value >= best_solution_value) and (unsafe_planes >= unsafe_planes_sol) and (num_of_crashes >= num_of_crashes_sol)) or ((unsafe_planes >= unsafe_planes_sol) and (num_of_crashes >= num_of_crashes_sol)) or (num_of_crashes >= num_of_crashes_sol):
+            return best_solution,best_solution_value
+        
+        best_solution = best_neighbor
+        best_solution_value = best_neighbor_value
+        unsafe_planes_sol = unsafe_planes
+        num_of_crashes_sol = num_of_crashes
         iteration += 1
-    return current_solution,current_cost
+    return best_solution,best_solution_value
 
 
 # Uso do algoritmo
